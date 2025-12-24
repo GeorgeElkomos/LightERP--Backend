12f339b488c3f970c84e11c5064b5f2ada72db92
samy1147 <100765882+samy1147@users.noreply.github.com>
2025-12-24 22:22:08 +0200

    update database configuration to use main db and remove experimental db

--- erp_project/settings.py
+++ erp_project/settings.py
@@ -108,7 +108,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db_experiment.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
